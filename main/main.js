6ef033fc7f8a2696ad5e06e095e00c4338e80178
xiaodao-zhang <542608536@qq.com>
2019-05-30 17:18:47 +0800

    taxi price

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var d_price;
+    var price;
+    var d=inputs.distance;
+    var p=inputs.parkTime;
+    if(d<=2)
+        d_price=6;
+    else if(d<=8)
+        d_price=6+(d-2)*0.8;
+    else
+        d_price=6+6*0.8+(d-8)*0.8*1.5;
+    price=d_price+p*0.25;
+    price=Math.round(price);
+    return price;
 };
